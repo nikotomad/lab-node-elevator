354d149a03cd29792aa40ecc3afd609ff7e79880
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-06 17:08:27 +0100

    Iterations 1 and 2 done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,53 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = 'Up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    var e = setInterval(() => {
+      this.update();
+    }, 500);
+  }
+
+  stop() {
+    console.log('update stopped');
+    clearInterval(e);
+  }
+
+  update() {
+    this.log();
+    if(this.floor < this.MAXFLOOR && this.direction == 'Up'){
+      this.floorUp();
+    } else {
+      if(this.floor == 0){
+        this.floorUp();
+      }
+      else {
+        this.floorDown();
+      }
+    }
+  }
+
   _passengersEnter() { }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+
+  floorUp() {
+    this.direction = 'Up';
+    this.floor += 1;
+  }
+
+  floorDown() {
+    this.direction = 'Down';
+    this.floor -= 1;
+  }
+
   call() { }
-  log() { }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,5 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+var elevator = new Elevator();
+elevator.start();
